a6473cff59a86f1f84bd6e38abc73442f12fd626
jeppeklh <jeppeklh@hotmail.com>
2023-11-09 16:36:17 +0100

    Subtraction-button now also correctly works as a negative-button

--- script.js
+++ script.js
@@ -6,19 +6,27 @@ const displayHistory = document.querySelector(".screen-top");
 const display = document.querySelector(".screen-bottom");
 const allKeys = document.querySelectorAll(".btn");
 const allClearKey = document.querySelector(".all-clear");
+const clearKey = document.querySelector(".clear");
 const numberKeys = document.querySelectorAll(".number");
 const operatorKeys = document.querySelectorAll(".operator");
+
 const decimalKey = document.querySelector(".decimal");
 const equalsKey = document.querySelector(".equals");
 
 allClearKey.addEventListener("click", () => {
-  display.innerHTML = "";
-  firstValue = "";
-  secondValue = "";
-  operator = "";
-  displayHistory.innerHTML = firstValue + " " + operator + " " + secondValue;
-  console.log(firstValue + " " + operator + " " + secondValue);
+  clear();
 });
+
+// clearKey.addEventListener("click", () => {
+//   if (firstValue != "" && operator === "") {
+//     firstValue.substring(0, firstValue.length - 1);
+//   }
+
+//   if (secondValue != "" && operator != "") {
+//     secondValue.substring(0, secondValue.length - 1);
+//   }
+//   showHistory();
+// });
 
 numberKeys.forEach((element) => {
   element.addEventListener("click", () => {
@@ -35,7 +43,8 @@ numberKeys.forEach((element) => {
 
 operatorKeys.forEach((element) => {
   element.addEventListener("click", () => {
-    if (firstValue != "" && operator == "") {
+    //also check for double minus
+    if (firstValue != "" && firstValue != "-" && operator == "") {
       display.innerHTML += element.innerHTML;
       operator = element.innerHTML;
       displayHistory.innerHTML =
@@ -56,21 +65,41 @@ operatorKeys.forEach((element) => {
 // });
 
 equalsKey.addEventListener("click", () => {
-  let answer = operate(
-    operator,
-    parseFloat(firstValue),
-    parseFloat(secondValue)
-  );
-  roundedAnswer = roundOf(answer);
-  display.innerHTML = roundedAnswer;
-  displayHistory.innerHTML =
-    firstValue + " " + operator + " " + secondValue + " = " + roundedAnswer;
-  firstValue = roundedAnswer;
-  operator = "";
-  secondValue = "";
+  if (operator != "" && secondValue != "") {
+    let answer = operate(
+      operator,
+      parseFloat(firstValue),
+      parseFloat(secondValue)
+    );
+    roundedAnswer = roundOf(answer);
+    if (firstValue == "" && secondValue == "") {
+      display.innerHTML = "";
+      displayHistory.innerHTML = "";
+    } else {
+      display.innerHTML = roundedAnswer;
+      displayHistory.innerHTML =
+        firstValue + " " + operator + " " + secondValue + " = " + roundedAnswer;
+      console.log(firstValue + " " + operator + " " + secondValue);
+      firstValue = roundedAnswer;
+      operator = "";
+      secondValue = "";
+    }
+  }
 });
 
 function showHistory() {
+  displayHistory.innerHTML = firstValue + " " + operator + " " + secondValue;
+}
+
+function updateDisplay() {
+  display.innerHTML = firstValue + " " + operator + " " + secondValue;
+}
+
+function clear() {
+  display.innerHTML = "";
+  firstValue = "";
+  secondValue = "";
+  operator = "";
   displayHistory.innerHTML = firstValue + " " + operator + " " + secondValue;
 }
 
@@ -110,6 +139,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
+    alert("You cant divide by zero");
+    clear();
     return null;
   }
 
@@ -118,8 +149,30 @@ function divide(a, b) {
 
 function modulo(a, b) {
   if (b === 0) {
+    alert("You cant divide by zero");
+    clear();
     return null;
   }
 
   return a % b;
 }
+
+//pseudo for subtract working as * -1
+// if first value is empty
+// first value += "-"
+
+// if operator id NOT empty and second value is empty
+// second value += "-"
+
+const minusButton = document.querySelector(".minus");
+
+minusButton.addEventListener("click", () => {
+  if (firstValue === "") {
+    firstValue += "-";
+  }
+  //can't use double minus
+  if (operator != "" && operator != "-" && secondValue === "") {
+    secondValue += "-";
+  }
+  updateDisplay();
+});
